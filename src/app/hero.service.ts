6bdd93be0c47ec9901e1db6d7cff10ee9dde7ced
Sergey Tsybulski <sergey.tsybulski@intexsoft.by>
2022-05-31 10:02:35 +0300

    add possibility to update hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,7 @@ import { of } from 'rxjs/internal/observable/of';
 import { Hero } from './model/hero';
 import {MessageService} from "./message.service";
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
@@ -13,6 +13,9 @@ import { catchError, tap } from 'rxjs/operators';
 export class HeroService implements Resolve<any> {
 
   private heroesUrl = 'api/heroes';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private http: HttpClient,
@@ -35,6 +38,13 @@ export class HeroService implements Resolve<any> {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
     );
   }
 
